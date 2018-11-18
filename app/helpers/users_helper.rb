328df95bb397280329127a437984e6221192702e
Карлов Андрей <qwenbk@bk.ru>
2018-11-18 22:13:05 +0300

    small revork

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
   def name(current_user)
-    sec = current_user.second_name
-    name, oth = "", ""
-    name = (current_user.first_name[0] + ".") if current_user.first_name.present?
-    oth = (current_user.last_name[0] + ".") if current_user.last_name.present?
-    sec + " " + name + oth
+    if (current_user.second_name)
+      sec = current_user.second_name
+      name, oth = "", ""
+      name = (current_user.first_name[0] + ".") if current_user.first_name.present?
+      oth = (current_user.last_name[0] + ".") if current_user.last_name.present?
+      sec + " " + name + oth
+    end
   end
 end
